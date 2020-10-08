5660880a6f0216b8ac9a85728e52b274dc588033
Akansha <36146495+ak-b@users.noreply.github.com>
2020-10-08 09:21:26 +0530

    Create read_call.py

---
+++ arrays_strings/read_call.py
@@ -0,0 +1,20 @@
+'''
+Given a file and assume that you can only read the file using a given method read4, implement a method read to read n characters. Your method read may be called multiple times.
+
+ 
+
+Method read4:
+
+The API read4 reads 4 consecutive characters from the file, then writes those characters into the buffer array buf.
+
+The return value is the number of actual characters read.
+
+Note that read4() has its own file pointer, much like FILE *fp in C.
+
+Definition of read4:
+
+    Parameter:  char[] buf4
+    Returns:    int
+
+Note: buf4[] is destination not source, the results from read4 will be copied to buf4[]
+'''
