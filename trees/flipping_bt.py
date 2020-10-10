212256777e971300a81065717c192d1e3d4829ca
Akansha <akansha.2389@gmail.com>
2020-10-11 01:15:08 +0530

    Create flipping_bt.py

---
+++ trees/flipping_bt.py
@@ -0,0 +1,24 @@
+'''
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivelent or false otherwise.
+'''
+class Solution:
+    def flipEquiv(self, root1, root2):
+        def dfs(node):
+            if node:
+                yield node.val
+                L = node.left.val if node.left else -1
+                R = node.right.val if node.right else -1
+                if L < R:
+                    yield from dfs(node.left)
+                    yield from dfs(node.right)
+                else:
+                    yield from dfs(node.right)
+                    yield from dfs(node.left)
+                yield '#'
+
+        return all(x == y for x, y in itertools.zip_longest(
+            dfs(root1), dfs(root2)))
