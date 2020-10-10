8e14c58725718f7668589e4d50edae331f3f38e6
Akansha <akansha.2389@gmail.com>
2020-10-11 00:09:18 +0530

    Update palindrome.py

--- dynamic_prog/palindrome.py
+++ dynamic_prog/palindrome.py
@@ -1,1 +1,14 @@
-
+'''
+Given a string s, return the longest palindromic substring in s.
+'''
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        m = ''  # Memory to remember a palindrome
+        for i in range(len(s)):  # i = start, O = n
+            for j in range(len(s), i, -1):  # j = end, O = n^2
+                if len(m) >= j-i:  # To reduce time
+                    break
+                elif s[i:j] == s[i:j][::-1]:
+                    m = s[i:j]
+                    break
+        return m
