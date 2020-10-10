0ae0de603710044487e0c62142a5e02d0ee0a511
Akansha <akansha.2389@gmail.com>
2020-10-11 00:23:30 +0530

    Create backspace_strcmp.py

---
+++ arrays_strings/backspace_strcmp.py
@@ -0,0 +1,30 @@
+'''
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+'''
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        def build(S):
+            ans = []
+            for c in S:
+                if c != '#':
+                    ans.append(c)
+                elif ans:
+                    ans.pop()
+            return "".join(ans)
+        return build(S) == build(T)
+  
+ class Solution(object):
+    def backspaceCompare(self, S, T):
+        def F(S):
+            skip = 0
+            for x in reversed(S):
+                if x == '#':
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x
+
+        return all(x == y for x, y in itertools.izip_longest(F(S), F(T)))
