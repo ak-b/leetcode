cc5d15fbe2de7457e5248fd188ae0149f46b375e
Akansha <akansha.2389@gmail.com>
2020-10-11 00:33:49 +0530

    Update expressive_words.py

--- arrays_strings/expressive_words.py
+++ arrays_strings/expressive_words.py
@@ -1,18 +1,16 @@
 '''
 Given a list of query words, return the number of words that are stretchy. 
 '''
-class Solution(object):
-    def expressiveWords(self, S, words):
-        def RLE(S):
-            return zip(*[(k, len(list(grp)))
-                         for k, grp in itertools.groupby(S)])
-
-        R, count = RLE(S)
-        ans = 0
-        for word in words:
-            R2, count2 = RLE(word)
-            if R2 != R: continue
-            ans += all(c1 >= max(c2, 3) or c1 == c2
-                       for c1, c2 in zip(count, count2))
-
-        return ans
+import itertools
+class Solution:
+    def expressiveWords(self, s: str, words: List[str]) -> int:
+        group = lambda x: [(k, len(list(v))) for k, v in itertools.groupby(x)]
+        S, count = group(s), 0
+        for W in map(group, words):
+            if len(W) != len(S): continue
+            for s, w in zip(S, W):
+                if s[0] != w[0] or s[1] < w[1] or w[1] < s[1] < 3:
+                    break
+            else:
+                count += 1
+        return count
