47c5a7e848e81e9f14e41ba3e53edb4ddef3aff9
Akansha <akansha.2389@gmail.com>
2020-10-11 00:56:23 +0530

    Create peak_index.py

---
+++ sortnsearch/peak_index.py
@@ -0,0 +1,26 @@
+'''
+Let's call an array arr a mountain if the following properties hold:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... arr[i-1] < arr[i]
+arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+Given an integer array arr that is guaranteed to be a mountain, return any i such that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+Example 1:
+Input: arr = [0,1,0]
+Output: 1
+
+Example 2:
+Input: arr = [0,2,1,0]
+Output: 1
+
+Example 3:
+Input: arr = [0,10,5,2]
+Output: 1
+'''
+class Solution(object):
+    def peakIndexInMountainArray(self, A):
+        for i in xrange(len(A)):
+            if A[i] > A[i+1]:
+                return i
