aed6b47386d3612ca3e4faadd813d7880f086848
Akansha <akansha.2389@gmail.com>
2020-10-11 00:37:27 +0530

    Create maxdist_pers.py

---
+++ arrays_strings/maxdist_pers.py
@@ -0,0 +1,26 @@
+'''
+You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 
+represents that the ith seat is empty (0-indexed).
+There is at least one empty seat, and at least one person sitting.
+Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized. 
+Return that maximum distance to the closest person.
+
+'''
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        people = (i for i, seat in enumerate(seats) if seat)
+        prev, future = None, next(people)
+
+        ans = 0
+        for i, seat in enumerate(seats):
+            if seat:
+                prev = i
+            else:
+                while future is not None and future < i:
+                    future = next(people, None)
+
+                left = float('inf') if prev is None else i - prev
+                right = float('inf') if future is None else future - i
+                ans = max(ans, min(left, right))
+
+        return ans
