280ec41df42e8c2056a2ed6508af4d8734935c4a
Akansha <akansha.2389@gmail.com>
2020-10-11 00:48:48 +0530

    Update median.py

--- sortnsearch/median.py
+++ sortnsearch/median.py
@@ -1,1 +1,44 @@
+'''
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
+Follow up: The overall run time complexity should be O(log (m+n)).
+'''
+class Solution:
+    def findMedianSortedArrays(self, A, B):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        # n>=m 
+        m, n = len(A),len(B)
+        if n<m:
+            A, B, m, n = B, A, n, m
+        
+        imin, imax, halfLen = 0, m, (m+n+1)/2
+        while(imin <= imax):
+            i = int((imin+imax)/2)
+            j = int(halfLen)-i
+            if i > 0 and A[i-1] > B[j]:
+                imax = i - 1
+            elif i < m and A[i] < B[j-1]:
+                imin = i + 1
+            else:
+                # i is perfect
+                if i==0:
+                    maxLeft = B[j-1]
+                elif j==0:
+                    maxLeft = A[i-1]
+                else:
+                    maxLeft = max(A[i-1], B[j-1])
+                
+                if (m+n) % 2 == 1:
+                    return maxLeft
+                else:
+                    if i==m:
+                        minRight = B[j]
+                    elif j==n:
+                        minRight = A[i]
+                    else:
+                        minRight = min(B[j],A[i])
+                return (maxLeft+minRight)/2
