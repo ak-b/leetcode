a80b9cffb7d10ae0cfaa9b636d915d9f8db94baf
Akansha <akansha.2389@gmail.com>
2020-10-10 23:52:58 +0530

    Create meeting_rooms.py

---
+++ arrays_strings/meeting_rooms.py
@@ -0,0 +1,93 @@
+'''
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+'''
+#Approach 1: Priority Queues
+'''
+Time Complexity: O(N\log N)O(NlogN).
+
+There are two major portions that take up time here. One is sorting of the array that takes O(NlogN) considering that the array consists of N elements.
+Then we have the min-heap. In t)he worst case, all N meetings will collide with each other. In any case we have NN add operations on the heap.
+In the worst case we will have N extract-min operations as well. Overall complexity being (NlogN) since extract-min operation on a heap takes O(logN).
+Space Complexity: O(N because we construct the min-heap and that can contain NN elements in the worst case as described above in the time complexity section.
+Hence, the space complexity is O(N).
+'''
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        # If there is no meeting to schedule then no room needs to be allocated.
+        if not intervals:
+            return 0
+
+        # The heap initialization
+        free_rooms = []
+
+        # Sort the meetings in increasing order of their start time.
+        intervals.sort(key= lambda x: x[0])
+
+        # Add the first meeting. We have to give a new room to the first meeting.
+        heapq.heappush(free_rooms, intervals[0][1])
+
+        # For all the remaining meeting rooms
+        for i in intervals[1:]:
+
+            # If the room due to free up the earliest is free, assign that room to this meeting.
+            if free_rooms[0] <= i[0]:
+                heapq.heappop(free_rooms)
+
+            # If a new room is to be assigned, then also we add to the heap,
+            # If an old room is allocated, then also we have to add to the heap with updated end time.
+            heapq.heappush(free_rooms, i[1])
+
+        # The size of the heap tells us the minimum rooms required for all the meetings.
+        return len(free_rooms)
+#Approach 2: Chronological Ordering
+'''
+Time Complexity: O(NlogN) because all we are doing is sorting the two arrays for start timings and end timings individually and 
+each of them would contain NN elements considering there are NN intervals.
+
+Space Complexity:O(N) because we create two separate arrays of size NN, one for recording the start times and one for the end times.
+'''
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        # If there are no meetings, we don't need any rooms.
+        if not intervals:
+            return 0
+
+        used_rooms = 0
+
+        # Separate out the start and the end timings and sort them individually.
+        start_timings = sorted([i[0] for i in intervals])
+        end_timings = sorted(i[1] for i in intervals)
+        L = len(intervals)
+
+        # The two pointers in the algorithm: e_ptr and s_ptr.
+        end_pointer = 0
+        start_pointer = 0
+
+        # Until all the meetings have been processed
+        while start_pointer < L:
+            # If there is a meeting that has ended by the time the meeting at `start_pointer` starts
+            if start_timings[start_pointer] >= end_timings[end_pointer]:
+                # Free up a room and increment the end_pointer.
+                used_rooms -= 1
+                end_pointer += 1
+
+            # We do this irrespective of whether a room frees up or not.
+            # If a room got free, then this used_rooms += 1 wouldn't have any effect. used_rooms would
+            # remain the same in that case. If no room was free, then this would increase used_rooms
+            used_rooms += 1    
+            start_pointer += 1   
+
+        return used_rooms
