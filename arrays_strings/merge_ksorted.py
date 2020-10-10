f62c79cf56a4ca65a18adde17b8e896ed01ade86
Akansha <akansha.2389@gmail.com>
2020-10-11 00:06:23 +0530

    Create merge_ksorted.py

---
+++ arrays_strings/merge_ksorted.py
@@ -0,0 +1,70 @@
+'''
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+'''
+#Merge with Divide And Conquer
+'''
+Intuition & Algorithm
+
+This approach walks alongside the one above but is improved a lot. We don't need to traverse most nodes many times repeatedly
+-Pair up k lists and merge each pair.
+-After the first pairing, k lists are merged into k/2 lists with average 2N/k length, then k/4, k/8 and so on.
+-Repeat this procedure until we get the final sorted linked list.
+-Thus, we'll traverse almost NN nodes per pairing and merging, and repeat this procedure about logk times.
+
+Time complexity : O(Nlogk) where \text{k}k is the number of linked lists.
+Space complexity : O(1)O(1)
+
+'''
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else None
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+        if not l1:
+            point.next=l2
+        else:
+            point.next=l1
+        return head.next
