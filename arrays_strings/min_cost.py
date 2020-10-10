014b1fef9e4032b292730741bd31798d8705a709
Akansha <akansha.2389@gmail.com>
2020-10-11 00:22:50 +0530

    Create min_cost.py

---
+++ arrays_strings/min_cost.py
@@ -0,0 +1,30 @@
+'''
+There are N workers.  The i-th worker has a quality[i] and a minimum wage expectation wage[i].
+
+Now we want to hire exactly K workers to form a paid group.  When hiring a group of K workers, we must pay them according to the following rules:
+
+Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
+Every worker in the paid group must be paid at least their minimum wage expectation.
+Return the least amount of money needed to form a paid group satisfying the above conditions.
+'''
+
+class Solution(object):
+    def mincostToHireWorkers(self, quality, wage, K):
+        from fractions import Fraction
+        workers = sorted((Fraction(w, q), q, w)
+                         for q, w in zip(quality, wage))
+
+        ans = float('inf')
+        pool = []
+        sumq = 0
+        for ratio, q, w in workers:
+            heapq.heappush(pool, -q)
+            sumq += q
+
+            if len(pool) > K:
+                sumq += heapq.heappop(pool)
+
+            if len(pool) == K:
+                ans = min(ans, ratio * sumq)
+
+        return float(ans)
