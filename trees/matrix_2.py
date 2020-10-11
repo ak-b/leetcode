f208810a21552b13c8ef602339a7375e7c67e0ce
Akansha <akansha.2389@gmail.com>
2020-10-11 22:27:59 +0530

    Create matrix_2.py

---
+++ trees/matrix_2.py
@@ -0,0 +1,33 @@
+'''
+On a 2D plane, we place stones at some integer coordinate points.  Each coordinate point may have at most one stone.
+Now, a move consists of removing a stone that shares a column or row with another stone on the grid.
+What is the largest possible number of moves we can make?
+'''
+class Solution(object):
+    def removeStones(self, stones):
+        graph = collections.defaultdict(list)
+        for i, x in enumerate(stones):
+            for j in range(i):
+                y = stones[j]
+                if x[0]==y[0] or x[1]==y[1]:
+                    graph[i].append(j)
+                    graph[j].append(i)
+
+        N = len(stones)
+        ans = 0
+
+        seen = [False] * N
+        for i in range(N):
+            if not seen[i]:
+                stack = [i]
+                seen[i] = True
+                while stack:
+                    ans += 1
+                    node = stack.pop()
+                    for nei in graph[node]:
+                        if not seen[nei]:
+                            stack.append(nei)
+                            seen[nei] = True
+                ans -= 1
+        return ans
+ 
