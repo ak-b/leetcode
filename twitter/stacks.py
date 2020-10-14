5f931cb55db1043420d15efc62805168b91152f3
Akansha <akansha.2389@gmail.com>
2020-10-14 08:57:03 +0530

    Create stacks.py

---
+++ twitter/stacks.py
@@ -0,0 +1,6 @@
+'''
+Create a simple stack which takes a list of elements.
+Each element contains a stack operator (push, pop, inc) and a value to either push/pop or two values, n and m,
+which increment the bottom n values by m.
+Then print the topmost value of the stack after every operation. If the stack is empty, print "empty"
+'''
