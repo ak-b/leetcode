ac5e04a8f68259d2797edf1fe1519a96ca4d8bf3
Akansha <akansha.2389@gmail.com>
2021-02-01 09:58:18 +0530

    Update missing_ranges.py

--- arrays_strings/missing_ranges.py
+++ arrays_strings/missing_ranges.py
@@ -63,34 +63,27 @@ When nums[i] - nums[i-1] > 1, we know that [nums[i-1] + 1, nums[i] - 1] range of
 # return ["2", "4->49", "51->74", "76->99"].
 #
 
-class Solution(object):
-    def findMissingRanges(self, nums, lower, upper):
-        """
-        :type nums: List[int]
-        :type lower: int
-        :type upper: int
-        :rtype: List[str]
-        """
-        def getRange(lower, upper):
-            if lower == upper:
-                return "{}".format(lower)
-            else:
-                return "{}->{}".format(lower, upper)
-        ranges = []
-        pre = lower - 1
-        
-        for i in xrange(len(nums) + 1):
-            if i == len(nums):
-                cur = upper + 1
-            else:
-                cur = nums[i]
-            if cur - pre >= 2:
-                ranges.append(getRange(pre + 1, cur - 1))
-                
-            pre = cur
-            
-        return ranges
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        result = []
 
+        def add(low, high):
+            if low > high:
+                return
+            if low == high:
+                result.append(str(low))
+            else:
+                result.append(str(low) + '->' + str(high))
 
-if __name__ == "__main__":
-    print Solution().findMissingRanges([0, 1, 3, 50, 75], 0, 99)
+        if not nums: # edge case
+            add(lower, upper)
+            return result
+
+        add(lower, nums[0] - 1)
+
+        for x in range(len(nums)):
+            add(nums[x - 1] + 1, nums[x] - 1)
+
+        add(nums[-1] + 1, upper)
+
+        return result
