52fa76ebb922aad45d8e17c265bf79ac07dc2546
Akansha <akansha.2389@gmail.com>
2021-02-11 10:58:31 +0530

    Create shuffle_array.py

---
+++ random/shuffle_array.py
@@ -0,0 +1,17 @@
+'''
+Given an integer array nums, design an algorithm to randomly shuffle the array.
+
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array.
+'''
+
+'''
+Intuition
+We can cut down the time and space complexities of shuffle with a bit of cleverness - namely, by swapping elements around within the array itself, we can avoid the linear space cost of the auxiliary array and the linear time cost of list modification.
+
+Algorithm
+The Fisher-Yates algorithm is remarkably similar to the brute force solution. On each iteration of the algorithm, we generate a random integer between the current index and the last index of the array. Then, we swap the elements at the current index and the chosen index - this simulates drawing (and removing) the element from the hat, as the next range from which we select a random index will not include the most recently processed one. One small, yet important detail is that it is possible to swap an element with itself - otherwise, some array permutations would be more likely than others. To see this illustrated more clearly, consider the animation below:
+'''
