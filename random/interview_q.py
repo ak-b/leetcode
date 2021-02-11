5b75c3318f9457128902cc25bd12faa2d6e76147
Akansha <akansha.2389@gmail.com>
2021-02-11 11:01:43 +0530

    Create interview_q.py

---
+++ random/interview_q.py
@@ -0,0 +1,21 @@
+'''
+Min Amplitude [New Grad] 🆕
+Ways to Split String [New Grad] 🆕
+Maximum Time ⭐⭐⭐ [Intern]
+Min Abs Difference of Server Loads ⭐ [Intern]
+Most Booked Hotel Room ⭐⭐⭐ [Intern]
+Minimum Domino Rotations For Equal Row [New Grad]
+Time to Type a String
+Maximum Level Sum of a Binary Tree
+Min Number of Chairs
+K Closest Points to Origin
+Odd Even Jump
+License Key Formatting
+Unique Email Addresses
+Fruit Into Baskets
+Min Days to Bloom
+Fill Matrix
+Decreasing Subsequences
+Max Distance
+Stores and Houses
+'''
