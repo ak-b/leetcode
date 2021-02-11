791e881a59899f5dad581ded9ae5eee5506e02a6
Akansha <akansha.2389@gmail.com>
2021-02-11 10:58:58 +0530

    Update shuffle_array.py

--- random/shuffle_array.py
+++ random/shuffle_array.py
@@ -15,3 +15,19 @@ We can cut down the time and space complexities of shuffle with a bit of clevern
 Algorithm
 The Fisher-Yates algorithm is remarkably similar to the brute force solution. On each iteration of the algorithm, we generate a random integer between the current index and the last index of the array. Then, we swap the elements at the current index and the chosen index - this simulates drawing (and removing) the element from the hat, as the next range from which we select a random index will not include the most recently processed one. One small, yet important detail is that it is possible to swap an element with itself - otherwise, some array permutations would be more likely than others. To see this illustrated more clearly, consider the animation below:
 '''
+
+class Solution:
+    def __init__(self, nums):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+        for i in range(len(self.array)):
+            swap_idx = random.randrange(i, len(self.array))
+            self.array[i], self.array[swap_idx] = self.array[swap_idx], self.array[i]
+        return self.array
